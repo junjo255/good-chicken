a816fbef9f73dd1b992fc253e08e12518197dae1
Eric Jo <43450544+junjo255@users.noreply.github.com>
2025-09-01 14:31:47 -0400

    he

--- src/app/MainRouter.tsx
+++ src/app/MainRouter.tsx
@@ -3,7 +3,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainScreen from "@/app/screens/MainScreen";
 import MenuScreen from "@/app/screens/MenuScreen";
-import CheckoutScreen from "@/app/screens/CheckoutScreen";
 
 export default function MainRouter({locations}: { locations: any[]; }) {
 
@@ -12,7 +11,7 @@ export default function MainRouter({locations}: { locations: any[]; }) {
             <Routes>
                 <Route path="/" element={<MainScreen locations={locations} />} />
                 <Route path="menu" element={<MenuScreen />} />
-                <Route path="checkout" element={<CheckoutScreen />} />
+                {/*<Route path="checkout" element={<CheckoutScreen />} />*/}
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </BrowserRouter>
