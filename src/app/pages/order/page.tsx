c9a0eddd38d63002b94af8670edff21abe07fe1b
Eric Jo <43450544+junjo255@users.noreply.github.com>
2025-09-04 10:48:59 -0400

    update

--- src/app/pages/order/page.tsx
+++ src/app/pages/order/page.tsx
@@ -2,14 +2,13 @@
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MenuScreen from "@/app/screens/MenuScreen";
-import CheckoutScreen from "@/app/screens/CheckoutScreen";
 
 export default function OrderRouterPage() {
     return (
         <BrowserRouter basename="/order">
             <Routes>
                 <Route path="/" element={<MenuScreen />} />
-                <Route path="checkout" element={<CheckoutScreen />} />
+                {/*<Route path="checkout" element={<CheckoutScreen />} />*/}
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </BrowserRouter>
